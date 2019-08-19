7dfe3e4aab1b7e7672a0c4cb9f3bf25d66446df3
wanyvic <q873040807@gmail.com>
2019-08-19 12:57:35 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+GITCOMMIT=$(git rev-parse --short HEAD)
+VERSION=$(git describe --abbrev=0 --tags)
+BUILDTIME=$(date -u -d "@${SOURCE_DATE_EPOCH:-$(date +%s)}" --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
+cat > prizesversion/version.go <<DVEOF
+// +build autogen
+// Package prizesversion is auto-generated at build-time
+package prizesversion
+// Default build-time variable for library-import.
+// This file is overridden on build with build-time information.
+const (
+	GitCommit             string = "$GITCOMMIT"
+	Version               string = "$VERSION"
+	BuildTime             string = "$BUILDTIME"
+)
+DVEOF
+go build -work ./cmd/prizesd/
